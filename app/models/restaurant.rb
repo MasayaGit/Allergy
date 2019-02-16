0fcb41d8814bc9eb1669443799a38ebd86c96961
TATSUHIRO <>
2019-02-17 01:43:53 +0900

    メニューの表示

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :comments, dependent: :destroy
+  has_many :menus, dependent: :destroy
   validates :r_name, presence: true
   validates :address, presence: true
   validates :genre, presence: true
