b92d717a0c5b04eb778237cfed7f876b4221d820
TATSUHIRO <>
2019-02-16 15:50:16 +0900

    Delete model & controller of Restaurant & Menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_054115) do
+ActiveRecord::Schema.define(version: 2019_02_16_061647) do
+
+  create_table "menus", force: :cascade do |t|
+    t.string "m_name"
+    t.integer "price"
+    t.string "allergy"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "r_name"
+    t.string "address"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
