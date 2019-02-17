205ab86a3dfdbfaa0873c244be8e4c0da11d9b17
TATSUHIRO <>
2019-02-17 23:56:53 +0900

    show (仮)

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,9 @@ class RestaurantsController < ApplicationController
 
     def show
       @restaurant = Restaurant.find(params[:id])
+      @allergys = User.find(2).allergy
+      # @time = Time.now.to_s
+      # 実際はログイン情報からとる
     end
 
 end
